8ad03bb93fe06153a4bbec5064eb1e1fd22bb686
wcastand <william@paybase.io>
2019-10-28 11:22:12 +0000

    fix(format): add prettier default

--- example/test4.tsx
+++ example/test4.tsx
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { Context } from 'koa'
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import { Context } from "koa";
+import styled from "styled-components";
 
-import { Div } from './utils/test.jsx'
+import { Div } from "./utils/test.jsx";
 
 const Button = styled.button`
   color: red;
   background: black;
-`
+`;
 
 type Props = {
-  hello: string
-}
+  hello: string;
+};
 
 const render = (props: Props) => {
-  const [state, setState] = useState(1000000)
+  const [state, setState] = useState(1000000);
   useEffect(() => {
-    import('./file2')
+    import("./file2")
       .then(m => m.one())
-      .then(console.log.bind(console))
-  })
+      .then(console.log.bind(console));
+  });
   return (
     <Div>
       <Button onClick={() => setState(x => x + 1)}>
         Hello {props.hello} {state}!
       </Button>
     </Div>
-  )
-}
+  );
+};
 
 export const getInitialProps = async (_: Context): Promise<Props> => {
   return {
-    hello: 'ofc! ofc! ofc! ofc! Typescript Boom Beam eekyie!',
-  }
-}
+    hello: "ofc! ofc! ofc! ofc! Typescript Boom Beam eekyie!"
+  };
+};
 
-export default render
+export default render;
--- packages/server/lib/utils.js
+++ packages/server/lib/utils.js
@@ -4,9 +4,9 @@
  *
  * This allows for `compose(mw1, compose(mw2, mw3))` to work as intended.
  */
-const isNext = Symbol('isNext')
+const isNext = Symbol("isNext");
 
-const isKoaNext = n => typeof n === 'function' && n.name === 'bound dispatch'
+const isKoaNext = n => typeof n === "function" && n.name === "bound dispatch";
 
 /**
  * `compose` is variadic and takes middlewares as input.
@@ -14,13 +14,13 @@ const isKoaNext = n => typeof n === 'function' && n.name === 'bound dispatch'
  * the middleware, currently `left-to-right`, returning a new `async` function.
  */
 const compose = (...mw) => async (...args) => {
-  const last = args[args.length - 1]
+  const last = args[args.length - 1];
   /**
    * The last `next` in the chain, should either call the `next` handler
    * passed via `args` (denoting a continuation into another composition),
    * or do a no-op.
    */
-  const nxt = last[isNext] || isKoaNext(last) ? args.pop() : () => {}
+  const nxt = last[isNext] || isKoaNext(last) ? args.pop() : () => {};
   /**
    * `await` execution of all the middleware provided, by reducing each
    * supplied middleware and wrapping each function execution.
@@ -31,38 +31,38 @@ const compose = (...mw) => async (...args) => {
        * Decorate each `next` handler with our `isNext` symbol to facilitate
        * composition of compositions.
        */
-      next[isNext] = true
-      await curr(...args.concat(next))
+      next[isNext] = true;
+      await curr(...args.concat(next));
     },
-    nxt,
-  )()
-}
+    nxt
+  )();
+};
 
 const uriToRegex = (() => {
-  const cache = new Map()
+  const cache = new Map();
 
   const constructRegex = uri =>
     `^${uri
-      .replace(/\$([^\/]+)/g, '(?<$1>[^/]+?)')
-      .replace(/\/index$/, '(?:/index)?')
-      .replace(/$/, '/?')
-      .replace(/\//g, '\\/')}$`
+      .replace(/\$([^\/]+)/g, "(?<$1>[^/]+?)")
+      .replace(/\/index$/, "(?:/index)?")
+      .replace(/$/, "/?")
+      .replace(/\//g, "\\/")}$`;
 
   return uri => {
     if (!cache.has(uri)) {
-      cache.set(uri, new RegExp(constructRegex(uri), 'i'))
+      cache.set(uri, new RegExp(constructRegex(uri), "i"));
     }
-    return cache.get(uri)
-  }
-})()
+    return cache.get(uri);
+  };
+})();
 
 const clearRequireCache = input =>
   Object.keys(require.cache)
-    .filter(c => c.includes(`.ofc/server/${input}/`) && !c.includes('node_modules'))
-    .forEach(s => delete require.cache[s])
+    .filter(c => c.includes(`.ofc/server/${input}/`) && !c.includes("node_modules"))
+    .forEach(s => delete require.cache[s]);
 
 module.exports = {
   compose,
   clearRequireCache,
-  uriToRegex,
-}
+  uriToRegex
+};
